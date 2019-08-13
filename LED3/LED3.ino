7c1704902fe773d9de197f763aeb0528f0e5706b
Efe Çıralı <50779159+efecirali@users.noreply.github.com>
2019-08-13 12:13:19 +0300

    Update LED3.ino

--- LED3/LED3.ino
+++ LED3/LED3.ino
@@ -1,9 +1,10 @@
-const int led1 = 5;
-const int led2 = 6;
-const int led3 = 7;
+// add const int ledx command to your led quantity
+const int led1 = 2;
+const int led2 = 3;
+const int led3 = 4;
 
 void setup() {
-
+  
 pinMode(led1,OUTPUT);
 pinMode(led2,OUTPUT);
 pinMode(led3,OUTPUT);
@@ -12,14 +13,16 @@ pinMode(led3,OUTPUT);
 
 void loop() {
   
-  analogWrite(led1,170);
-  analogWrite(led2,170);
-  analogWrite(led3,170);
-  delay(1900);
-  analogWrite(led1,0);
-  analogWrite(led2,0);
-  analogWrite(led3,0);
-  delay(1300);
+ //you may change brightness from analogWrite(ledx,brightnessvalue)(between 1-190)
+ //you may change delay time by millisecond type
+ analogWrite(led1,170);
+ analogWrite(led2,170);
+ analogWrite(led3,170);
+ delay(1900);
+ analogWrite(led1,0);
+ analogWrite(led2,0);
+ analogWrite(led3,0);
+ delay(1300);
 
 }
 
